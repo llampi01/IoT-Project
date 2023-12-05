48b51a59a2d3e5ed7cf5cd71edeaf94acd8a9fb8
llampi01 <63289406+llampi01@users.noreply.github.com>
2023-12-05 11:04:38 +0200

    Add files via upload

---
+++ Anomaly Detection/unusualTraffic.c
@@ -0,0 +1,89 @@
+#include "contiki.h"
+#include "net/routing/routing.h"
+#include "random.h"
+#include "net/netstack.h"
+#include "net/ipv6/simple-udp.h"
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdio.h> // Include stdio for snprintf
+
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+#define WITH_SERVER_REPLY  1
+#define UDP_CLIENT_PORT	8765
+#define UDP_SERVER_PORT	5678
+
+#define SEND_INTERVAL		  (10 * CLOCK_SECOND)
+
+static struct simple_udp_connection udp_conn;
+static uint32_t rx_count = 0;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(udp_client_process, "UDP client");
+AUTOSTART_PROCESSES(&udp_client_process);
+/*---------------------------------------------------------------------------*/
+static void
+udp_rx_callback(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
+  LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
+  LOG_INFO_6ADDR(sender_addr);
+  LOG_INFO_("\n");
+  rx_count++;
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udp_client_process, ev, data)
+{
+  static struct etimer periodic_timer;
+  static char str[250]; // Adjust the size of the buffer to accommodate the maximum payload
+  uip_ipaddr_t dest_ipaddr;
+  static uint32_t tx_count;
+  static uint32_t missed_tx_count;
+
+  PROCESS_BEGIN();
+
+  /* Initialize UDP connection */
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
+                      UDP_SERVER_PORT, udp_rx_callback);
+
+  etimer_set(&periodic_timer, SEND_INTERVAL);
+  while(1) {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+
+    if(NETSTACK_ROUTING.node_is_reachable() &&
+        NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
+
+      /* Send to DAG root */
+      LOG_INFO("Sending request %"PRIu32" to ", tx_count);
+      LOG_INFO_6ADDR(&dest_ipaddr);
+      LOG_INFO_("\n");
+
+      // Generate random payload size between 70 and 250 bytes
+      int payload_size = (random_rand() % (250 - 70 + 1)) + 70;
+
+      snprintf(str, sizeof(str), "hello %" PRIu32 " with size %d", tx_count, payload_size);
+      simple_udp_sendto(&udp_conn, str, payload_size, &dest_ipaddr); // Send with the generated payload size
+      tx_count++;
+    } else {
+      LOG_INFO("Not reachable yet\n");
+      if(tx_count > 0) {
+        missed_tx_count++;
+      }
+    }
+
+    /* Add some jitter */
+    etimer_set(&periodic_timer, SEND_INTERVAL
+      - CLOCK_SECOND + ((2 * CLOCK_SECOND)));
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+
